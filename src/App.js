82f039290298780a4458490b47bd1e82a6c0aacc
John Leintz <johnleintz@scaledflow.com>
2020-09-18 16:41:01 -0500

    added usereducer sample code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from 'react';
+// useReducer is similar to redux
+// useReducer is like useState but 
+// typically will use objects rather than values
+// https://www.youtube.com/watch?v=kK_Wqx3RnHk
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+import React, { useReducer } from 'react';
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
 }
 
-export default App;
+// way one
+// function reducer(state, action) {
+//   return { count: state.count + 1 }
+// }
+
+function reducer(state, action) {
+  // console.log(action.type);
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      console.log(state);
+      return { count: state.count + 1 }
+    case ACTIONS.DECREMENT:
+    return { count: state.count - 1 }
+    default: 
+      return state
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+
+  // way one
+  // function increment() {
+  //   dispatch()
+  // }
+
+  function increment() {
+    dispatch({ type: ACTIONS.INCREMENT })
+  }
+
+  function decrement() {
+    dispatch({ type: ACTIONS.DECREMENT })
+  }
+  
+  return (
+    <>
+      <button onClick={decrement}>-</button>
+      {/* {console.log(state)} */}
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
+    </>
+  );
+}
